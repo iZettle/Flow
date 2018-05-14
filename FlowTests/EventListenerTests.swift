622a91f1aa8755a6e37286f83b12d8d8a86b3ec0
Måns Bernhardt <mans@izettle.com>
2018-05-14 08:51:39 +0200

    Added unit test testHasEnablableEventListeners

---
+++ FlowTests/EventListenerTests.swift
@@ -0,0 +1,42 @@
+//
+//  EventListenerTests.swift
+//  FlowTests
+//
+//  Created by Måns Bernhardt on 2018-05-14.
+//  Copyright © 2018 iZettle. All rights reserved.
+//
+
+import XCTest
+import Flow
+
+class EventListenerTests: XCTestCase {
+    func testHasEnablableEventListeners() {
+        let listeners: [Listener] = [(false, false), (true, false), (false, true), (true, true)].map {
+            let l = Listener()
+            l.isEnabled = $0
+            l.hasEventListeners = $1
+            return l
+        }
+        
+        XCTAssertEqual(listeners.map { $0.isEnabled }, [false, true, false, true])
+        
+        let d = listeners.disableActiveEventListeners()
+
+        XCTAssertEqual(listeners.map { $0.isEnabled }, [false, true, false, false])
+        
+        d.dispose()
+
+        XCTAssertEqual(listeners.map { $0.isEnabled }, [false, true, false, true])
+    }
+}
+
+
+class Listener: HasEventListeners, Enablable {
+    var isEnabled = false
+    var hasEventListeners = false
+}
+
+extension Array: HasEnablableEventListeners where Element: HasEventListeners & Enablable {
+    public var enablableEventListeners: [Enablable & HasEventListeners] { return self }
+}
+
