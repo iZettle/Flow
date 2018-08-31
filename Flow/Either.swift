e31360891f025cec08b606cd9333aff45e6477b0
Måns Bernhardt <mans.bernhardt@gmail.com>
2018-08-31 11:33:00 +0200

    Conditionally conform `Either` to Hashable when `Left` and `Right` conforms to `Hashable`.

--- Flow/Either.swift
+++ Flow/Either.swift
@@ -74,3 +74,12 @@ extension Either: Equatable where Left: Equatable, Right: Equatable {
         }
     }
 }
+
+extension Either: Hashable where Left: Hashable, Right: Hashable {
+    public var hashValue: Int {
+        switch self {
+        case .left(let left): return left.hashValue
+        case .right(let right): return right.hashValue
+        }
+    }
+}
